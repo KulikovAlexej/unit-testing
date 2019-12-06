95c27e003cb58093e72f8a08e5be5115ef79f3e1
Куликов Алексей <aleksey.kulikov.96@gmail.com>
2019-12-06 19:03:59 +0300

    simple test of form

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,11 @@
+import { FormBuilder } from '@angular/forms';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
 
   beforeEach(() => {
-    component = new CounterComponent();
+    component = new CounterComponent(new FormBuilder());
   });
 
   // beforeAll, afterEach, afterAll
@@ -26,5 +27,30 @@ describe('CounterComponent', () => {
     component.increment();
 
     expect(result).toBe(1);
+  });
+
+  it('form has login and email field', () => {
+    expect(component.form.contains('login')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  });
+
+  it('should invalid if form is empty', () => {
+    expect(component.form.invalid).toBeTruthy();
+  });
+
+  it('should correct if form has login', () => {
+    const form = component.form;
+    const loginCtrl = form.get('login');
+    loginCtrl.setValue('Alex96');
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('should uncorrect if form has incorrect email', () => {
+    const form = component.form;
+    const emailCtrl = form.get('email');
+    const loginCtrl = form.get('login');
+    loginCtrl.setValue('Alex96');
+    emailCtrl.setValue('alex@mail');
+    expect(component.form.valid).toBeTruthy();
   });
 });
